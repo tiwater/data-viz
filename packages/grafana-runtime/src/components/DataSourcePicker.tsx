080348162f3c7d588d9389066421d210287e0d02
李世正 <lishizheng@tiwater.com>
2023-03-01 18:38:58 +0800

    fix: 删除 console

--- packages/grafana-runtime/src/components/DataSourcePicker.tsx
+++ packages/grafana-runtime/src/components/DataSourcePicker.tsx
@@ -181,7 +181,6 @@ export class DataSourcePicker extends PureComponent<DataSourcePickerProps, DataS
       // 解决编辑面板数据图图片无法找到问题
       imgUrl: `https://data-viz.ticos.cn/${o.imgUrl}`,
     }));
-    console.log('optionsoptions', options);
     const value = this.getCurrentValue();
     const isClearable = typeof onClear === 'function';
 
