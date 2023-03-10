51cf15acd89c96a3facf1202164f4284f91e568d
李世正 <lishizheng@tiwater.com>
2023-03-10 11:54:03 +0800

    fix: 国际化面板库

--- public/app/features/library-panels/components/UnlinkModal/UnlinkModal.tsx
+++ public/app/features/library-panels/components/UnlinkModal/UnlinkModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ConfirmModal } from '@grafana/ui';
+import { t } from 'app/core/internationalization';
 
 interface Props {
   isOpen: boolean;
@@ -11,11 +12,16 @@ interface Props {
 export const UnlinkModal = ({ isOpen, onConfirm, onDismiss }: Props) => {
   return (
     <ConfirmModal
-      title="Do you really want to unlink this panel?"
+      title={t(
+        'features.library-panels.do-you-really-want-to-unlink-this-panel',
+        'Do you really want to unlink this panel?'
+      )}
       icon="question-circle"
-      body="If you unlink this panel, you will be able to edit it without affecting any other dashboards.
-            However, once you make a change you will not be able to revert to its original reusable panel."
-      confirmText="Yes, unlink"
+      body={t(
+        'features.library-panels.if-you-unlink-this-panel',
+        'If you unlink this panel, you will be able to edit it without affecting any other dashboards. However, once you make a change you will not be able to revert to its original reusable panel.'
+      )}
+      confirmText={t('features.library-panels.yes-unlink', 'Yes, unlink')}
       onConfirm={() => {
         onConfirm();
         onDismiss();
